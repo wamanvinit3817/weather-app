28049dd86e2869cd6ed7ebdda183a2675ddb6fca
Vineet Waman <wamanvinit8717@gmail.com>
2025-08-15 16:26:14 +0530

    Update package.json and vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Change REPO_NAME to your repository name
 export default defineConfig({
   plugins: [react()],
+  base: '/weather-app/'
 })
